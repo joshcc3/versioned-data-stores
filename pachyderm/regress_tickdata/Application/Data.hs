49181750d511a12aea3ea92ccee713b01901084b
jrc12 <jrc12@ic.ac.uk>
2018-04-29 11:16:40 +0100

    Added outline of the structure

--- pachyderm/regress_tickdata/Application/Data.hs
+++ pachyderm/regress_tickdata/Application/Data.hs
@@ -2,7 +2,7 @@ module Data where
 
 type Check a = a -> Either (Err (m, a)) a
 
-data Data m a = Data { checks :: Check (m, a), metadata :: m, price :: a }
+data Data m a = Data { checks :: Check (m, a), metadata :: m, dat :: a }
 
 
 
--- pachyderm/regress_tickdata/Application/Main.hs
+++ pachyderm/regress_tickdata/Application/Main.hs
@@ -14,20 +14,40 @@ import Data.Csv
 
 type ClosePrice = Price
 
+
 type DataSample = Tree [PathComponent] ClosePrice
+
+
 type Observed = DataSample
 type Expected = DataSample
-
-
 type Binned = Map Bin Observed
-
-
 type Evaluated = Map Bin Score
-
 type ScoreAsCSV = CSVRecord
-
 type FPath = String
 type OutputRecord = Rec
 
 
+observed :: String -> IO Expected
+observed = undefined
 
+binned :: String -> IO Binned
+binned = undefined
+
+evaluate :: Expected -> Binned -> Evaluated
+evaluate = undefined
+
+csvRecord :: Evaluated -> Map FileName [CSVRecord]
+csvRecord = undefined
+
+
+writeRecord :: FileName -> [Rec] -> IO ()
+writeRecord = undefined
+
+
+main :: IO ()
+main = do
+  expected <- observed
+  bins <- binned
+  evaluated <- evaluate expected bins
+  recs <- csvRecord evaluated
+  mapM_ (M.toList recs) (\(fname, csvrecs) -> writeRecord fname (dat csvrecs))
